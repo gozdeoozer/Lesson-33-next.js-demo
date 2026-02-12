624fe06a9c5ad6de94c0fc87eb1386425f30338b
gozdeoozer <gozdeoozer@icloud.com>
2026-02-12 14:00:11 +0800

    commit third changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,31 +1,29 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import { QuotesProvider } from "@/app/QuotesContext";
-import "./globals.css";
+import { Geist, Geist_Mono } from 'next/font/google';
+import { QuotesProvider } from '@/app/QuotesContext';
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata = {
-  title: "Random Quotes App",
-  description: "A simple app to display random quotes",
+  title: 'Random Quotes App',
+  description: 'A simple app that displays random quotes.',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <QuotesContext.Provider>
-          {children}
-        </QuotesContext.Provider>
+        <QuotesProvider>{children}</QuotesProvider>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,57 +1,20 @@
 'use client';
 
-import { useState } from 'react';
-import { quotes as initialQuotes } from '@/quotes';
 import { Subtitle } from '@/components/Subtitle';
 import { Body2 } from '@/components/Body2';
 import { Button } from '@/components/Button';
+import { useQuotesContext, useQuotesDispatchContext } from '@/app/QuotesContext';
 
-
-
-function getRandomIndex(currentIndex, arrayLength) {
-  const random = Math.floor(Math.random() * arrayLength);
-
-  const result = random;
-  if (result === currentIndex) {
-    getRandomIndex(currentIndex, arrayLength);
-  } else {
-    return result;
-  }
-}
 export default function Home() {
-  const [quotes, setQuotes] = useState(initialQuotes);
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  function handleNextClick() {
-    console.log('next quote is clicked');
-    const nextIndex = getRandomIndex(currentIndex, quotes.length);
-    setCurrentIndex(nextIndex);
-  }
-
-  function handleLikeClick() {
-    console.log("Like button clicked");
-    setQuotes((prevQuotes) => {
-      const newQuotes = [...prevQuotes];
-
-      const currentQuote = newQuotes[currentIndex];
-      newQuotes[currentIndex] = {
-        ...currentQuote,
-        likeCount: (currentQuote.likeCount || 0) + 1,
-      };
-
-      return newQuotes;
-    });
-  }
-
+  const { quotes, currentIndex } = useQuotesContext();
+  const { handleNextQuoteClick } = useQuotesDispatchContext();
 
   return (
     <main className='min-h-dvh flex items-center'>
       <div className='w-md mx-auto bg-slate-700 p-10 rounded-md flex flex-col'>
         <Subtitle title={quotes[currentIndex].quote} />
         <Body2>{quotes[currentIndex].author}</Body2>
-        <Button
-          onClick={handleNextClick}>Next Quote</Button>
-        <Button onClick={handleLikeClick}>Like ({quotes[currentIndex].likeCount})</Button>
+        <Button onClick={handleNextQuoteClick}>Next Quote</Button>
       </div>
     </main>
   );
