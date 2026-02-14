b10a3b3cd365b2bc8f4e6b1228839bcc4c34ae76
gozdeoozer <gozdeoozer@icloud.com>
2026-02-14 19:16:28 +0800

    commit 3.changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from 'next/font/google';
 import { QuotesProvider } from '@/app/QuotesContext';
+import { UserProvider } from '@/app/UserContext';
 import './globals.css';
+
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -23,7 +25,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <QuotesProvider>{children}</QuotesProvider>
+        <UserProvider>
+          <QuotesProvider>{children}</QuotesProvider>
+        </UserProvider>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,13 @@ import { Subtitle } from '@/components/Subtitle';
 import { Body2 } from '@/components/Body2';
 import { Button } from '@/components/Button';
 import { useQuotesContext, useQuotesDispatchContext } from '@/app/QuotesContext';
+import { useUserContext } from '@/app/UserContext';
 
 
 export default function Home() {
   const { quotes, currentIndex } = useQuotesContext();
   const { handleNextQuoteClick } = useQuotesDispatchContext();
+  const user = useUserContext();
 
   return (
     <main className='min-h-dvh flex items-center'>
@@ -16,6 +18,7 @@ export default function Home() {
         <Subtitle title={quotes[currentIndex].quote} />
         <Body2>{quotes[currentIndex].author}</Body2>
         <Button onClick={handleNextQuoteClick}>Next Quote</Button>
+        <Body2>{user.name}</Body2>
       </div>
     </main>
   );
